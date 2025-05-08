882da460eb9abe98c5e14e40b2eff0e53a2a79b4
Frances-Cockburn <fcockbu1@jhu.edu>
2025-05-08 00:54:41 -0400

    Create imports.py

---
+++ inferKat3/imports.py
@@ -0,0 +1,14 @@
+import scanpy as sc
+import numpy as np
+import pandas as pd
+import anndata as ad
+import infercnvpy as cnv
+import matplotlib.pyplot as plt
+import warnings
+import seaborn as sns
+
+warnings.simplefilter("ignore")
+
+sc.settings.set_figure_params(figsize=(5, 5))
+
+sc.logging.print_header()
