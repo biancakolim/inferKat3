6cb1ffd23ce702fbdcc05cd2d0ccb590d6248b9f
Frances-Cockburn <fcockbu1@jhu.edu>
2025-05-08 01:26:50 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='inferKat2',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'scanpy',
+        'scipy',
+        'umap-learn',
+        'leidenalg',
+        'infercnvpy',
+        'hmmlearn', 
+        'pykalman',
+    ],
+    author='CEKN',
+    description='CSCB CEKN Final Project Package',
+)
